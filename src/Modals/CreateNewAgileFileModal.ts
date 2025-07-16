cf101bb4fc7d59dd3097dbbd02cfcfff394e8fdc
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-07-15 23:12:05 -0400

    Cleanup and Formaty

--- src/Modals/CreateNewAgileFileModal.ts
+++ src/Modals/CreateNewAgileFileModal.ts
@@ -43,7 +43,7 @@ class CreateMewAgileFileModal extends Modal {
 
 
         //let epycBtn = div.createEl('img', { cls: "image", attr: { src: this.GetEpicSVG(), alt: 'Epyc' } });
-       // let storyBtn = div.createEl('img', { cls: "image", attr: { src: this.GetStorySVG(), alt: 'Story' } });
+        // let storyBtn = div.createEl('img', { cls: "image", attr: { src: this.GetStorySVG(), alt: 'Story' } });
         //let taskBtn = div.createEl('img', { cls: "image", attr: { src: this.GetTaskSVG(), alt: 'Task' } });
 
         epycBtn.addEventListener('click', () => {
@@ -69,49 +69,26 @@ class CreateMewAgileFileModal extends Modal {
         contentEl.empty();
     }
 
-//     private GetEpicSVG(): string {
-//         return `<svg width="200" height="200" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-//   <rect width="24" height="24" rx="4" fill="#A259FF"/>
-//   <path d="M13 2L6 13H11L11 22L18 11H13L13 2Z" fill="white"/>
-// </svg>`;
-//     }
-
-//     private GetStorySVG(): string {
-//         return `<svg width="200" height="200" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-//   <rect width="24" height="24" rx="4" fill="#4CD964"/>
-//   <path d="M6 4C6 2.9 6.9 2 8 2H16C17.1 2 18 2.9 18 4V21L12 17L6 21V4Z" fill="white"/>
-// </svg>`;
-//     }
-
-//     private GetTaskSVG(): string {
-//         return `<svg width="200" height="200" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
-//   <rect width="24" height="24" rx="4" fill="#32ADE6"/>
-//   <path d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7L19.59 5.59L9 16.17Z" fill="white"/>
-// </svg>`;
-//     }
-
-private GetEpicSVG(): string {
-    return `<svg viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
+    private GetEpicSVG(): string {
+        return `<svg viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
   <rect width="24" height="24" rx="4" fill="#A259FF"/>
   <path d="M13 2L6 13H11L11 22L18 11H13L13 2Z" fill="white"/>
 </svg>`;
-}
+    }
 
-private GetStorySVG(): string {
-    return `<svg viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
+    private GetStorySVG(): string {
+        return `<svg viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
   <rect width="24" height="24" rx="4" fill="#4CD964"/>
   <path d="M6 4C6 2.9 6.9 2 8 2H16C17.1 2 18 2.9 18 4V21L12 17L6 21V4Z" fill="white"/>
 </svg>`;
-}
+    }
 
-private GetTaskSVG(): string {
-    return `<svg viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
+    private GetTaskSVG(): string {
+        return `<svg viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%;">
   <rect width="24" height="24" rx="4" fill="#32ADE6"/>
   <path d="M9 16.17L4.83 12L3.41 13.41L9 19L21 7L19.59 5.59L9 16.17Z" fill="white"/>
 </svg>`;
-}
-
-
+    }
 }
 
 export default CreateMewAgileFileModal;
